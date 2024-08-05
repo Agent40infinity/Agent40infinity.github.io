f468b44dbd64d3555283d5001419677e8ac96f1b
Aiden Nathan <agent9@y7mail.com>
2024-08-05 23:25:31 +1000

    fix route

---
+++ Components/Project.cs
@@ -0,0 +1,13 @@
+﻿namespace aiden.fyi.Components
+{
+    public class Project
+    {
+        public string title { get; set; } = "";
+        public bool displayTitle { get; set; } = true;
+        public string description { get; set; } = "";
+        public string image { get; set; } = "";
+        public string[] key { get; set; }
+        public string[] utility { get; set; }
+        public string link { get; set; } = "";
+    }
+}
