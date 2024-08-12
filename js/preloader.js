0292039b9bce1d75a438f3ed585a2b1e588018bb
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2024-08-12 14:30:29 +0000

    Deploying to release from  @ f898d5b8ce648b3e7425e00c2130d26dcf9fc5a6 🚀

--- js/preloader.js
+++ js/preloader.js
@@ -1,7 +1,32 @@
 function Rendered(duration) {
+    OnImagesLoaded(duration);
+}
+
+function Loaded(duration) {
     $('.pre-loader').fadeOut(duration);
 }
 
 function Unloaded(duration) {
     $('.pre-loader').fadeIn(duration);
+}
+
+function OnImagesLoaded(duration) {
+    var images = document.getElementsByTagName("img");
+    var loaded = images.length;
+    for (var i = 0; i < images.length; i++) {
+        if (images[i].complete) {
+            loaded--;
+        }
+        else {
+            images[i].addEventListener("load", function () {
+                loaded--;
+                if (loaded <= 0) {
+                    Loaded(duration);
+                }
+            });
+        }
+    }
+    if (loaded <= 0) {
+        Loaded(duration);
+    }
 }
