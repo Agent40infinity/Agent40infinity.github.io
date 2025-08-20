8ba2e3eab50256a7ad774e9396cadf8f082f1120
Aiden Nathan <agent9@y7mail.com>
2025-08-21 00:16:46 +1000

    ADDED - json.NET package and lazy loading for projects and resume databases. 1 time load per website crawl.

--- Components/Data.cs
+++ Components/Data.cs
@@ -1,16 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Text.Json;
 
 namespace aiden.fyi.Components
 {
     public static class Data
     {
         public static Dictionary<string, List<Project>> Projects = new Dictionary<string, List<Project>>();
-        public static Dictionary<string, object> Resume;
+        public static Dictionary<string, JsonElement> Resume = new Dictionary<string, JsonElement>();
 
-        public static List<SkillItem> Languages => (List<SkillItem>)Resume["language"];
-        public static List<SkillItem> Legend => (List<SkillItem>)Resume["legend"];
-        public static List<SoftwareCategory> Software => (List<SoftwareCategory>)Resume["software"];
-        public static List<ExperienceCategory> Experience => (List<ExperienceCategory>)Resume["experience"];
-        public static List<Study> Education => (List<Study>)Resume["education"];
+        public static List<SkillItem>? Languages => JsonConvert.DeserializeObject<List<SkillItem>>(Resume["language"].ToString());
+        public static List<SkillItem>? Legend => JsonConvert.DeserializeObject<List<SkillItem>>(Resume["legend"].ToString());
+        public static List<SoftwareCategory>? Software => JsonConvert.DeserializeObject<List<SoftwareCategory>>(Resume["software"].ToString());
+        public static List<ExperienceCategory>? Experience => JsonConvert.DeserializeObject<List<ExperienceCategory>>(Resume["experience"].ToString());
+        public static List<Study>? Education => JsonConvert.DeserializeObject<List<Study>>(Resume["education"].ToString());
     }
 }
