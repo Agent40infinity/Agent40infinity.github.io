38bd9aa9c330acca1463c0aff64f2704ade21035
Aiden Nathan <agent9@y7mail.com>
2025-08-20 13:15:57 +1000

    Another annoying test

--- Components/Data.cs
+++ Components/Data.cs
@@ -5,24 +5,12 @@ namespace aiden.fyi.Components
     public static class Data
     {
         public static Dictionary<string, List<Project>> Projects = new Dictionary<string, List<Project>>();
+        public static Dictionary<string, object> Resume = new Dictionary<string, object>();
 
-        public static List<SkillItem> languages = new List<SkillItem>();
-        public static List<SkillItem> legend = new List<SkillItem>();
-        public static List<Components.SoftwareCategory> software = new List<Components.SoftwareCategory>();
-        public static List<Components.ExperienceCategory> experience = new List<Components.ExperienceCategory>();
-        public static List<Study> education = new List<Study>();
-
-        public static Dictionary<string, object> Resume
-        {
-            set
-            {
-                languages = (List<SkillItem>)value["language"];
-                legend = (List<SkillItem>)value["legend"];
-                software = (List<Components.SoftwareCategory>)value["software"];
-                experience = (List<Components.ExperienceCategory>)value["experience"];
-                education = (List<Study>)value["education"];
-            }
-
-        }
+        public static List<SkillItem> Languages => (List<SkillItem>)Resume["language"];
+        public static List<SkillItem> Legend => (List<SkillItem>)Resume["legend"];
+        public static List<SoftwareCategory> Software => (List<SoftwareCategory>)Resume["software"];
+        public static List<ExperienceCategory> Experience => (List<ExperienceCategory>)Resume["experience"];
+        public static List<Study> Education => (List<Study>)Resume["education"];
     }
 }
