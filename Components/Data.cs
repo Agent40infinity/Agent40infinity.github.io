c1e24836ffe241bb999a1cd02c0dca40eae016ff
Aiden Nathan <agent9@y7mail.com>
2025-08-20 16:59:50 +1000

    How the fuck do I manage to break the order of operations on everything I touch

--- Components/Data.cs
+++ Components/Data.cs
@@ -5,7 +5,7 @@ namespace aiden.fyi.Components
     public static class Data
     {
         public static Dictionary<string, List<Project>> Projects = new Dictionary<string, List<Project>>();
-        public static Dictionary<string, object> Resume = new Dictionary<string, object>();
+        public static Dictionary<string, object> Resume;
 
         public static List<SkillItem> Languages => (List<SkillItem>)Resume["language"];
         public static List<SkillItem> Legend => (List<SkillItem>)Resume["legend"];
