bdd5f2a078533a13ed5b5663974ecf5dc3913c7e
Aiden Nathan <agent9@y7mail.com>
2025-08-20 12:31:41 +1000

    Haha resume why you no FUCKING TYPE CAST PROPERLY

---
+++ Components/Data.cs
@@ -0,0 +1,8 @@
+﻿namespace aiden.fyi.Components
+{
+    public static class Data
+    {
+        public static Dictionary<string, List<Project>> Projects = new Dictionary<string, List<Project>>();
+        public static Dictionary<string, object> Resume = new Dictionary<string, object>();
+    }
+}
