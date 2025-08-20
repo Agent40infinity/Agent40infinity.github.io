edc5246c52cb7c9c635f26b42ad82094b96644b0
Aiden Nathan <agent9@y7mail.com>
2025-08-20 12:46:53 +1000

    Thinking is brain damaging rn

--- Components/Data.cs
+++ Components/Data.cs
@@ -1,8 +1,28 @@
-﻿namespace aiden.fyi.Components
+﻿using System.Collections.Generic;
+
+namespace aiden.fyi.Components
 {
     public static class Data
     {
         public static Dictionary<string, List<Project>> Projects = new Dictionary<string, List<Project>>();
-        public static Dictionary<string, object> Resume = new Dictionary<string, object>();
+
+        public static List<SkillItem> languages = new List<SkillItem>();
+        public static List<SkillItem> legend = new List<SkillItem>();
+        public static List<Components.SoftwareCategory> software = new List<Components.SoftwareCategory>();
+        public static List<Components.ExperienceCategory> experience = new List<Components.ExperienceCategory>();
+        public static List<Study> education = new List<Study>();
+
+        public static Dictionary<string, object> Resume
+        {
+            set
+            {
+                languages = (List<SkillItem>)value["language"];
+                legend = (List<SkillItem>)value["legend"];
+                software = (List<Components.SoftwareCategory>)value["software"];
+                experience = (List<Components.ExperienceCategory>)value["experience"];
+                education = (List<Study>)value["education"];
+            }
+
+        }
     }
 }
